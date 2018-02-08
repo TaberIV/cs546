ae47ab687797a420c0ce7a0eaa001d0bb4e8161c
Taber McFarlin <tabermcfarlin@gmail.com>
2018-02-07 21:56:39 -0500

    No more sudoku

--- Labs/Lab3/app.js
+++ Labs/Lab3/app.js
@@ -1,4 +1,21 @@
 const fileData = require("./fileData");
 const textMetrics = require("./textMetrics");
 
+async function main(path) {
+	try {
+		var data = await fileData.getFileAsString(path);
+		console.log(data);
+	} catch (error) {
+		throw error;
+	}
+}
+
+console.log("Test begin.");
+
+try {
+	main();
+} catch (error) {
+	throw error;
+}
+
 console.log("Test complete.");
--- Labs/Lab3/fileData.js
+++ Labs/Lab3/fileData.js
@@ -1,7 +1,19 @@
+const bluebird = require("bluebird");
+const Promise = bluebird.Promise;
+const fs = bluebird.promisifyAll(require("fs"));
+
+//Get the file as a string
 async function getFileAsString(path) {
+	if (!path || typeof path !== 'string')
+		throw "You must provide a path as a string";
 
+	try {
+		const data = await fs.readFileAsync(path, "utf8");
+		return data;
+	} catch (error) {
+		throw error;
+	}
 }
-
 async function getFileAsJSON(path) {
 
 }
@@ -19,4 +31,4 @@ module.exports = {
 	getFileAsJSON,
 	saveStringToFile,
 	saveJSONToFile
-}
+};
