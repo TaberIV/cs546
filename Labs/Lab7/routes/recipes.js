7b4e58c931110bb54f7e3d719c5bc3e224fe3db6
Taber McFarlin <tabermcfarlin@gmail.com>
2018-03-10 03:18:05 -0500

    Lab 7 done?

--- Labs/Lab7/routes/recipes.js
+++ Labs/Lab7/routes/recipes.js
@@ -3,63 +3,87 @@ const router = express.Router();
 const recipeData = require("../data/recipes");
 
 router.get("/", async (req, res) => {
-    try {
-        const recipeList = await recipeData.getAllRecipes();
-        res.json(recipeList);
-    } catch (e) {
-        console.log(e);
-        res.status(500).json({error: e});
-    }
+	try {
+		const recipeList = await recipeData.getAllRecipes();
+		res.json(recipeList);
+	} catch (e) {
+		console.log(e);
+		res.status(500).json({error: e});
+	}
 });
 
 router.get("/:id", async(req, res) => {
-    try {
-        let recipe = await recipeData.getRecipeById(req.params.id);
-        res.json(recipe); 
-    } catch (e) {
-        res.status(404).json({error: "Recipe not found"});
-    }
+	try {
+		let recipe = await recipeData.getRecipeById(req.params.id);
+		res.json(recipe); 
+	} catch (e) {
+		console.log(e);
+		res.status(404).json({error: "Recipe not found"});
+	}
 });
 
 router.post("/", async(req, res) => {
-    const newRecipeData = req.body;
- 
-    try {
-        let createdRecipe = await recipeData.createRecipe(newRecipeData.title, newRecipeData.ingredients, newRecipeData.steps);
-        res.json(createdRecipe);
-    } catch (e) {
-        res.status(500).json({error: e});
-    }
+	const newRecipeData = req.body;
+
+	try {
+		let createdRecipe = await recipeData.createRecipe(newRecipeData.title, newRecipeData.ingredients, newRecipeData.steps);
+		res.json(createdRecipe);
+	} catch (e) {
+		console.log(e);
+		res.status(500).json({error: e});
+	}
 });
 
 router.put("/:id", async(req, res) => {
-    try {
-        await recipeData.getRecipeByID(req.params.id);
-        try {
-            const newRecipeData = req.body;
-            
-            let updatedRecipe = await recipeData.updateRecipe(req.params.id, newRecipeData);
-            res.json(updatedRecipe);
-        } catch (e) {
-            res.status(500).json({error: e});
-        }
-    } catch (e) {
-        res.status(404).json({error: "Recipe not found"});
-    }
+	try {
+		await recipeData.getRecipeById(req.params.id);
+		try {
+			const newRecipeData = req.body;
+			
+			let updatedRecipe = await recipeData.replaceRecipe(req.params.id, newRecipeData);
+			res.json(updatedRecipe);
+		} catch (e) {
+		console.log(e);
+			res.status(500).json({error: e});
+		}
+	} catch (e) {
+		console.log(e);
+		res.status(404).json({error: "Recipe not found"});
+	}
+});
+
+router.patch("/:id", async(req, res) => {
+	try {
+		await recipeData.getRecipeById(req.params.id);
+		try {
+			const newRecipeData = req.body;
+			
+			let updatedRecipe = await recipeData.updateRecipe(req.params.id, newRecipeData);
+			res.json(updatedRecipe);
+		} catch (e) {
+		console.log(e);
+			res.status(500).json({error: e});
+		}
+	} catch (e) {
+		console.log(e);
+		res.status(404).json({error: "Recipe not found"});
+	}
 });
 
 router.delete("/:id", async(req, res) => {
-    try {
-        await recipeData.getRecipeByID(req.params.id);
-        try {
-            let deletionInfo = await recipeData.removeRecipe(req.params.id);
-            res.json(deletionInfo);
-        } catch (e) {
-            res.status(500).json({ error: e });
-        }
-    } catch (e) {
-        res.status(404).json({ error: "Recipe not found" });
-    }
+	try {
+		await recipeData.getRecipeById(req.params.id);
+		try {
+			let deletionInfo = await recipeData.deleteRecipe(req.params.id);
+			res.json(deletionInfo);
+		} catch (e) {
+		console.log(e);
+			res.status(500).json({ error: e });
+		}
+	} catch (e) {
+		console.log(e);
+		res.status(404).json({ error: "Recipe not found" });
+	}
 });
 
 
